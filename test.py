bd41e95c78acbe78540923308b3ecf0bb1bd1cc5
Ding Huo <cancer061218@gmail.com>
2024-05-30 00:29:17 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-# Load the newly uploaded numpy array from the file with allow_pickle set to True
-new_data = np.load('data\points\example\example_points.npy', allow_pickle=True)
-
-# Display the newly loaded data
-print(new_data)
